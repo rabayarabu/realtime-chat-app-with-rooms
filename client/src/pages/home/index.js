5ba5a1e4a7e362f51c827781c64a6423beb54fb5
rabayarabu <rabu9149@gmail.com>
2023-08-29 00:54:52 +0600

    Change home page theme

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -15,7 +15,7 @@ const Home = ({ username, setUsername, room, setRoom, socket }) => {
   return (
     <div className={styles.container}>
       <div className={styles.formContainer}>
-        <h1>{`<>DevRooms</>`}</h1>
+        <h1>{`Rooms:)`}</h1>
         <input className={styles.input} placeholder='Username...'
         onChange={(e) => setUsername(e.target.value)} />
 
