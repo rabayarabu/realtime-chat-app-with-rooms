39246f9390f4b856aa0e42259c6bc012e70deec5
rabayarabu <rabu9149@gmail.com>
2023-08-28 19:36:53 +0600

    Conect socket io with app

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
-import { useState } from 'react'; // Add this
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import io from 'socket.io-client'; // Add this
+import { useState } from 'react';
 import Home from './pages/home';
+import Chat from './pages/chat';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import io from 'socket.io-client';
 
-const socket = io.connect('http://localhost:4000'); // Add this -- our server will run on port 4000, so we connect to it from here
+const socket = io.connect('http://localhost:4000');
 
 function App() {
-  const [username, setUsername] = useState(''); // Add this
-  const [room, setRoom] = useState(''); // Add this
+  const [username, setUsername] = useState('');
+  const [room, setRoom] = useState('');
 
   return (
     <Router>
@@ -18,13 +19,18 @@ function App() {
             path='/'
             element={
               <Home
-                username={username} // Add this
-                setUsername={setUsername} // Add this
-                room={room} // Add this
-                setRoom={setRoom} // Add this
-                socket={socket} // Add this
+                username={username}
+                setUsername={setUsername}
+                room={room}
+                setRoom={setRoom}
+                socket={socket}
               />
             }
+          />
+          {/* Add this */}
+          <Route
+            path='/chat'
+            element={<Chat username={username} room={room} socket={socket} />}
           />
         </Routes>
       </div>
--- server/index.js
+++ server/index.js
@@ -1,53 +1,36 @@
 const express = require('express');
 const app = express();
-const http = require('http');
+http = require('http');
 const cors = require('cors');
 const { Server } = require('socket.io');
 
 app.use(cors()); // Add cors middleware
-
-
 const server = http.createServer(app);
-app.get('/', (req, res) => {
-    res.send('Hello world');
+// Create an io server and allow for CORS from http://localhost:3000 with GET and POST methods
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+// Listen for when the client connects via socket.io-client
+io.on('connection', (socket) => {
+  console.log(`User connected ${socket.id}`);
+
+  // We can write our socket event listeners in here...
+  // Add a user to a room
+  socket.on('join_room', (data) => {
+    const { username, room } = data; // Data sent from client when join_room event emitted
+    socket.join(room); // Join the user to a socket room
+
+    let __createdtime__ = Date.now(); // Current timestamp
+    // Send message to all users currently in the room, apart from the user that just joined
+    socket.to(room).emit('receive_message', {
+      message: `${username} has joined the chat room`,
+      username: CHAT_BOT,
+      __createdtime__,
+    });
   });
+});
 
 server.listen(4000, () => 'Server is running on port 4000');
-const io = new Server(server, {
-    cors: {
-      origin: 'http://localhost:3000',
-      methods: ['GET', 'POST'],
-    },
-  });
-  const CHAT_BOT = 'ChatBot';
-  let chatRoom = ''; // E.g. javascript, node,...
-  let allUsers = []; // All users in current chat room
-
-  io.on('connection', (socket) => {
-    console.log(`User connected ${socket.id}`);
-  
-    // We can write our socket event listeners in here...
-    socket.on('join_room', (data) => {
-        const { username, room } = data; // Data sent from client when join_room event emitted
-        socket.join(room); // Join the user to a socket room
-      });
-      
-      let __createdtime__ = Date.now();
-      socket.to(room).emit('receive_message', {
-        message: `${username} has joined the chat room`,
-        username: CHAT_BOT,
-        __createdtime__,
-      });
-      socket.emit('receive_message', {
-        message: `Welcome ${username}`,
-        username: CHAT_BOT,
-        __createdtime__,
-      });
-       // Save the new user to the room
-        chatRoom = room;
-        allUsers.push({ id: socket.id, username, room });
-        chatRoomUsers = allUsers.filter((user) => user.room === room);
-        socket.to(room).emit('chatroom_users', chatRoomUsers);
-        socket.emit('chatroom_users', chatRoomUsers);
-  });
- 
